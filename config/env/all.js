224a23a7586a15c71d425714076ca4d7126c701b
manasmehrotra3 <manasmehrotra3@gmail.com>
2016-05-01 00:19:17 -0400

    port update

--- config/env/all.js
+++ config/env/all.js
@@ -6,7 +6,7 @@ module.exports = {
 		description: 'My persoanl web application',
 		keywords: 'manas, mehrotra, manas mehrotra, manasmehrotra'
 	},
-	port: process.env.PORT || 3000,
+	port: process.env.PORT || 8080,
 	templateEngine: 'swig',
 	sessionSecret: 'MEAN',
 	sessionCollection: 'sessions',
